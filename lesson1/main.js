c576204b78e91236dad997c4cdf2cc3c905c129c
KlebanAndrii <smokymolla888@gmail.com>
2021-03-25 00:31:59 +0200

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,357 @@
+// 1- присвоїти кожному з наступних значень свою змінну: 'hello', 'owu', 'com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вивести кожну за допомогою console.log , alert, document.write
+
+/*
+let str = 'hello';
+console.log(str);
+alert(str);
+document.write(str);
+
+let str1 = 'owu';
+console.log(str1);
+alert(str1);
+document.write(str1);
+
+let str2 = 'com';
+console.log(str2);
+alert(str2);
+document.write(str2);
+
+let str3 = 'ua';
+console.log(str3);
+alert(str3);
+document.write(str3);
+
+let num = 1;
+console.log(num);
+alert(num);
+document.write(num);
+
+let num1 = 10;
+console.log(num1);
+alert(num1);
+document.write(num1);
+
+let num2 = -999;
+console.log(num2);
+alert(num2);
+document.write(num2);
+
+let num3 = 123;
+console.log(num3);
+alert(num3);
+document.write(num3);
+
+let num4 = 3.14;
+console.log(num4);
+alert(num4);
+document.write(num4);
+
+let num5 = 2.7;
+console.log(num5);
+alert(num5);
+document.write(num5);
+
+let num6 = 16;
+console.log(num6);
+alert(num6);
+document.write(num6);
+
+let str4 = true;
+console.log(str4);
+alert(str4);
+document.write(str4);
+
+let str5 = false;
+console.log(str5);
+alert(str5);
+document.write(str5);
+*/
+
+// 2- перевизначити кожну змінну із завдання 1 давши їм довільні значення Вивести кожну за допомогою console.log, alert, document.write
+
+/*
+str = 'one';
+console.log(str);
+alert(str);
+document.write(str);
+
+str1 = 'two';
+console.log(str1);
+alert(str1);
+document.write(str1);
+
+str2 = 'three';
+console.log(str2);
+alert(str2);
+document.write(str2);
+
+str3 = 'four';
+console.log(str3);
+alert(str3);
+document.write(str3);
+*/
+
+/*num = 2;
+console.log(num);
+alert(num);
+document.write(num);*/
+
+/*num1 = 12;
+console.log(num1);
+alert(num1);
+document.write(num1);
+
+num2 = -40;
+console.log(num2);
+alert(num2);
+document.write(num2);
+
+num3 = 321;
+console.log(num3);
+alert(num3);
+document.write(num3);
+
+num4 = 2.12;
+console.log(num4);
+alert(num4);
+document.write(num4);
+
+num5 = 1983;
+console.log(num5);
+alert(num5);
+document.write(num5);
+
+num6 = 37;
+console.log(num6);
+alert(num6);
+document.write(num6);
+
+str4 = 100;
+console.log(str4);
+alert(str4);
+document.write(str4);
+
+str5 = -100;
+console.log(str5);
+alert(str5);
+document.write(str5);*/
+
+// 3- Створити 3 числових і 3 стрінгових константи. Вивести кожну за допомогою console.log, alert, document.write
+/*const NUMBER1 = 2;
+console.log(NUMBER1);
+alert(NUMBER1);
+document.write(NUMBER1);
+
+const NUMBER2 = 12;
+console.log(NUMBER2);
+alert(NUMBER2);
+document.write(NUMBER2);
+
+const NUMBER3 = 1983;
+console.log(NUMBER3);
+alert(NUMBER3);
+document.write(NUMBER3);
+
+const STRING1 = 'Hello';
+console.log(STRING1);
+alert(STRING1);
+document.write(STRING1);
+
+const STRING2 = 'HowAreYou';
+console.log(STRING2);
+alert(STRING2);
+document.write(STRING2);
+
+const STRING3 = 'Bye';
+console.log(STRING3);
+alert(STRING3);
+document.write(STRING3);*/
+
+// 4- за допомогою 3х різних prompt () отримати 3 слова які є вашими ПІБ. Для прізвища імені та по батькові створити різні змінні. Вивести кожну за допомогою console.log, alert, document.write
+
+/*
+const surName1 = prompt('Kleban');
+console.log(surName1);
+
+const Name1 = prompt('Andrii');
+console.log(Name1);
+
+const fatName1 = prompt('Bogdanovich');
+console.log(fatName1);
+
+let surname1 = 'Kleban';
+console.log(surname1);
+alert(surname1);
+document.write(surname1);
+
+let name1 = 'Andrii';
+console.log(name1);
+alert(name1);
+document.write(name1);
+
+let fatname1 = 'Bogdanovich';
+console.log(fatname1);
+alert(fatname1);
+document.write(fatname1);
+*/
+
+// 5- Взяти змінні із завдання 4 і сконкатеніровать їх в одній змінної person.////////////////////////////////////////
+
+/*let person = surname1 + ' ' + name + ' ' + fatname1;
+console.log(person);*/
+
+// 6- Взяти задні 4 і 5 і застосувати його до ПІБ всіх членів своєї родини.///////////////////////////////////////////
+
+/*const surName2 = prompt('Kleban');
+console.log(surName2);
+
+const Name2 = prompt('Maryana');
+console.log(Name2);
+
+const fatName2 = prompt('Mykhailivna');
+console.log(fatName2);
+
+let surname2 = 'Kleban';
+console.log(surname2);
+alert(surname2);
+document.write(surname2);
+
+let name2 = 'Maryana';
+console.log(name2);
+alert(name2);
+document.write(name2);
+
+let fatname2 = 'Mykhailivna';
+console.log(fatname2);
+alert(fatname2);
+document.write(fatname2);
+
+let SurNameFat2 = surname2 + ' ' + name2 + ' ' + fatname2;
+console.log(SurNameFat2);
+
+const surName3 = prompt('Kleban');
+console.log(surName3);
+
+const Name3 = prompt('Veronika');
+console.log(Name3);
+
+const fatName3 = prompt('Andriivna');
+console.log(fatName3);
+
+let surname3 = 'Kleban';
+console.log(surname3);
+alert(surname3);
+document.write(surname3);
+
+let name3 = 'Veronika';
+console.log(name3);
+alert(name3);
+document.write(name3);
+
+let fatname3 = 'Andriivna';
+console.log(fatname3);
+alert(fatname3);
+document.write(fatname3);
+
+let SurNameFat3 = surname3 + ' ' + name3 + ' ' + fatname3;
+console.log(SurNameFat3);*/
+
+// 7- за допомогою prompt () отримати 3 числа. Привести їх до числового типу за допомогою +. вивести їх в консоль.////
+
+/*let number1 = +prompt('number 2');
+let number2 = +prompt('number 12');
+let number3 = +prompt('number 83');
+console.log(number1, number2, number3);*/
+
+// 8- за допомогою prompt () отримати 4 числа. Привести їх до числового типу за допомогою parseInt. Скласти їх між собою записавши результат в змінну result і вивести в консоль браузера
+
+/*
+let number01 = prompt('par 10');
+let number02 = prompt('par 20');
+let number03 = prompt('par 30');
+let number04 = prompt('par 40');
+
+let parseInt1 = parseInt(number01);
+let parseInt2 = parseInt(number02);
+let parseInt3 = parseInt(number03);
+let parseInt4 = parseInt(number04);
+
+let result = parseInt1 + parseInt2 + parseInt3 + parseInt4;
+console.log(result);*/
+
+// 9- за допомогою prompt () отримати 3 числа з плаваючою точок. за допомогою parseFloat привести їх до допомоги на відповідне типу. Скласти їх між собою записавши результат в змінну result і вивести в консоль браузера
+
+/*let point1 = prompt('float 30.1');
+let point2 = prompt('float 20.2');
+let point3 = prompt('float 10.3');
+
+let float1 = parseFloat(point1);
+let float2 = parseFloat(point2);
+let float3 = parseFloat(point3);
+
+let result = float1 + float2 + float3;
+console.log(result);*/
+
+// 10- за допомогою prompt () отримати 3 числа з плаваючою точок. Округлити їх за допомогою Math.round Скласти їх між собою записавши результат в змінну і вивести в консоль браузера
+
+/*
+let float4 = prompt('round off 1');
+let numBer11 = parseFloat(float4);
+let round1 = Math.round(numBer11);
+
+let float5 = prompt('round off 2');
+let numBer22 = parseFloat(float5);
+let round2 = Math.round(numBer22);
+
+let float6 = prompt('round off 3');
+let numBer33 = parseFloat(float6);
+let round3 = Math.round(numBer33);
+
+console.log(round1 + round2 + round3);*/
+
+// 11- за допомогою prompt () отримати 2 цілих числа. Привести їх до целочисленному типу. Перше число - це число яке будуть підносити до степеня. Друге число - це число яке є ступенем. За допомогою Math.pow звести перше число в ступінь другого числа.
+
+/*
+let num = prompt('pow 2')
+let pow = prompt('pow 4');
+let NumPow = Math.pow(num, pow);
+console.log(NumPow);*/
+
+// 12- За допомогою оператора опредеоенного типу typeof визначити типи наступних змінних і вивести їх в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+/*let a = 100;
+console.log(a);
+let b = '100';
+console.log(b);
+let c = true;
+console.log(c);
+let d = undefined;
+console.log(d);*/
+
+// 13- Поставте відповідний оператор в виразах що б вийшов відповідний результат.
+//  В однакових виразах в завданні не використовувати два однакових оператора.
+// Кожен вираз вкласти в свою змінну
+
+let numOne = 5 < 6/*true*/
+console.log(numOne);
+let numTwo = 5 > 6  /*false*/
+console.log(numTwo);
+let numThree = 5 == 6 /*false*/
+console.log(numThree);
+let numFour = 5 === 6  /*false*/
+console.log(numFour);
+let numFive = 10 <= 10 /*true*/
+console.log(numFive);
+let numSix = 10 >= 10 /*true*/
+console.log(numSix);
+let numSeven = 10 != 10  /*false*/
+console.log(numSeven);
+let numEight = 10 > 10 /*false*/
+console.log(numEight);
+let numNine = 10 < 10  /*false*/
+console.log(numNine);
+let numTen = 123 === '123' /*false*/
+console.log(numTen);
+let numEleven = 123 == '123'  /*true*/
+console.log(numEleven);
+
